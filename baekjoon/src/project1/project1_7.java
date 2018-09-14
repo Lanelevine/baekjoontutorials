8c93dce4d515f7afa9902a4d52899f9bb73bd337
zlfoo <zlfoo@DESKTOP-HVOSQDN>
2018-09-14 18:26:03 +0900

    baekjoon tutorials

---
+++ baekjoon/src/project1/project1_7.java
@@ -0,0 +1,32 @@
+package project1;
+
+// Tip!! : ArrayList 함수와 스캐너 함수를 같이 써주자
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class project1_7 {
+
+	public static void main(String[] args) {
+		/* 문제 : 입력 받은 대로 출력하는 프로그램을 작성하시오.
+		 * 입력 : 입력이 주어진다. 입력은 최대 100줄로 이루어져 있고, 알파벳 소문자, 대문자, 공백, 숫자로만 이루어져 있다.
+		 * 		각 줄은 100글자를 넘지 않으며, 빈 줄은 주어지지 않는다. 또, 각 줄은 공백으로 시작하지 않고, 공백으로 끝나지 않는다.
+		 */
+		Scanner sc= new Scanner(System.in);
+        ArrayList<String> inputs = new ArrayList<>();
+         
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if (input == null || input.isEmpty()) {
+                break;
+            }
+            inputs.add(input);
+        }
+        sc.close();
+ 
+        for (int i = 0; i < inputs.size(); ++i) {
+            System.out.println(inputs.get(i));
+        }
+
+	}
+
+}
